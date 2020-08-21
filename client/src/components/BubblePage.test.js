f92a6bdb76ae62a9c1b5b785873d4a8749d361cf
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-08-21 13:38:34 -0400

    i feel stupid

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,30 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import BubblePage from "./BubblePage";
+import React from 'react'
+import { render } from '@testing-library/react'
+import BubblePage from './BubblePage'
+import axios from 'axios'
+import axiosWithAuth from './axiosWithAuth'
 
-test("Fetches data and renders the bubbles", () => {
-  // Finish this test
-});
+jest.mock('react')
+jest.mock('axios')
+
+describe('this is crap...', () => {
+	test('Gets and renders bubbles', async () => {
+		render(<BubblePage />)
+		const mockdata = [ {
+      color: "aliceblue",
+      code: {
+        hex: "#f0f8ff"
+      },
+      id: 1
+    },
+    {
+      color: "limegreen",
+      code: {
+        hex: "#99ddbc"
+      },
+      id: 2
+    }, ]
+		axios.create.mockImplementationOnce(() => Promise.resolve(mockdata))
+		await expect(axiosWithAuth()).resolves.toEqual(mockdata)
+	})
+})
